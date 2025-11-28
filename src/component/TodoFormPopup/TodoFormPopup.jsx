eaa2b353d7a071b2cb55bb59bb020d3b5c50ba78
Nyasha <nyashacodes@gmail.com>
2025-11-28 09:50:59 +0530

    css changed

--- src/component/TodoFormPopup/TodoFormPopup.jsx
+++ src/component/TodoFormPopup/TodoFormPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./todoFormPopup.css";
+import "./TodoFormPopup.css";
 
 function TodoFormPopup({ onClose, todos, setTodos, editingTodo }) {
   const [task, setTask] = useState("");
