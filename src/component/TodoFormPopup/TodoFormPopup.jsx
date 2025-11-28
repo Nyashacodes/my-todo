24c20c46ee4f5e7261927e0f7fb5be29929f32bf
Nyasha <nyashacodes@gmail.com>
2025-11-28 10:06:28 +0530

    fix: corrected TodoFormPopup.css import

--- src/component/TodoFormPopup/TodoFormPopup.jsx
+++ src/component/TodoFormPopup/TodoFormPopup.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./TodoFormPopup.css";
 
 function TodoFormPopup({ onClose, todos, setTodos, editingTodo }) {
   const [task, setTask] = useState("");
